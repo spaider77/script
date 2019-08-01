0b9edb79b973417344ab9a8dc627267222dc71b4
Ivan Topor <49291467+l4ckyguy@users.noreply.github.com>
2019-08-01 23:17:27 +0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,9 +1,7 @@
 
 #!/bin/sh
 
-
-sudo nano /etc/apt/sources.list
-deb http://dl.google.com/linux/chrome/deb/ stable main
+echo 'deb http://dl.google.com/linux/chrome/deb/ stable main'>>/etc/apt/sources.list
 wget https://dl-ssl.google.com/linux/linux_signing_key.pub
 sudo apt-key add linux_signing_key.pub
 sudo apt-get update
