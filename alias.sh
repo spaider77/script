3ac7aba8b09a024c068291abecc2842b970bf3da
Ivan Topor <49291467+l4ckyguy@users.noreply.github.com>
2019-10-01 06:58:02 +0300

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo 'echo HELLLO!'>/usr/bin/hello && chmod +x /usr/bin/hello
+#launch many copies telegram-desktop
+useradd -m -G sudo -s /bin/bash x1 && chown -hR x1 /home/x1/
+useradd -m -G sudo -s /bin/bash x2 && chown -hR x2 /home/x2/
+useradd -m -G sudo -s /bin/bash x3 && chown -hR x3 /home/x3/
+
+cat << EOF > /usr/bin/tgk
+#!/bin/bash
+read -p 'number: ' number
+gksu -u x$number telegram-desktop
+EOF
+
+chmod +x /usr/bin/tgk
