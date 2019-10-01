4e0c9776302428a29b7404aef30041f69ba59e12
Ivan Topor <49291467+l4ckyguy@users.noreply.github.com>
2019-10-01 07:06:56 +0300

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -7,6 +7,10 @@ useradd -m -G sudo -s /bin/bash x2
 chown -hR x2 /home/x2/
 useradd -m -G sudo -s /bin/bash x3 
 chown -hR x3 /home/x3/
+useradd -m -G sudo -s /bin/bash x4 
+chown -hR x4 /home/x4/
+useradd -m -G sudo -s /bin/bash x5 
+chown -hR x5 /home/x5/
 
 touch /usr/bin/tgk
 echo 'gksu -u x$1 telegram-desktop &'>/usr/bin/tgk
