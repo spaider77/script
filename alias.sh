52daa68575a5cc6eb369433dc705defc23b7a4cc
Ivan Topor <49291467+l4ckyguy@users.noreply.github.com>
2019-10-01 07:36:24 +0300

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -11,3 +11,8 @@ sudo chown -hR x3 /home/x3/
 sudo touch /usr/bin/tgk
 sudo echo 'gksu -u x$1 telegram-desktop &'>/usr/bin/tgk
 sudo chmod +x /usr/bin/tgk
+
+#run smsbomber
+touch /usr/bin/bomber
+echo "ssh -t root@88.99.163.122 -p 50400 sudo /opt/infinite-bomber-reborn/builds/linux/Infinite-Bomber-x64/infinite-bomber">/usr/bin/bomber
+chmod +x /usr/bin/bomber
