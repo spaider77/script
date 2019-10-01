b163f072fed6a579b21a422296ff2daad5fb9881
Ivan Topor <49291467+l4ckyguy@users.noreply.github.com>
2019-10-01 07:09:25 +0300

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #launch many copies telegram-desktop
-useradd -m -G sudo -s /bin/bash x1 
-chown -hR x1 /home/x1/
-useradd -m -G sudo -s /bin/bash x2 
-chown -hR x2 /home/x2/
-useradd -m -G sudo -s /bin/bash x3 
-chown -hR x3 /home/x3/
-useradd -m -G sudo -s /bin/bash x4 
-chown -hR x4 /home/x4/
-useradd -m -G sudo -s /bin/bash x5 
-chown -hR x5 /home/x5/
+sudo useradd -m -G sudo -s /bin/bash x1 
+sudo chown -hR x1 /home/x1/
+sudo useradd -m -G sudo -s /bin/bash x2 
+sudo chown -hR x2 /home/x2/
+sudo useradd -m -G sudo -s /bin/bash x3 
+sudo chown -hR x3 /home/x3/
+sudo useradd -m -G sudo -s /bin/bash x4 
+sudo chown -hR x4 /home/x4/
+sudo useradd -m -G sudo -s /bin/bash x5 
+sudo chown -hR x5 /home/x5/
 
-touch /usr/bin/tgk
-echo 'gksu -u x$1 telegram-desktop &'>/usr/bin/tgk
-chmod +x /usr/bin/tgk
+sudo touch /usr/bin/tgk
+sudo echo 'gksu -u x$1 telegram-desktop &'>/usr/bin/tgk
+sudo chmod +x /usr/bin/tgk
