50661c66478a873ef4e2a4ee9229ed9499327735
Ivan Topor <49291467+l4ckyguy@users.noreply.github.com>
2019-10-07 13:16:15 +0300

    Update term.sh

--- term.sh
+++ term.sh
@@ -41,16 +41,16 @@ echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
 
 #Net-Hunter
 #install chroot
-cd
-curl -LO https://raw.githubusercontent.com/Hax4us/Nethunter-In-Termux/master/kalinethunter
-tsudo chmod +x kalinethunter
-tsudo ./kalinethunter --insecure
+#cd
+#curl -LO https://raw.githubusercontent.com/Hax4us/Nethunter-In-Termux/master/kalinethunter
+#tsudo chmod +x kalinethunter
+#tsudo ./kalinethunter --insecure
 
-echo ''
-echo ''
-echo 'now run nethunter with tsudo startkali and put some commands there:'
-echo ''
-echo 'apt update && apt install wget openssl'
-echo 'wget https://http.kali.org/kali/pool/main/k/kali-archive-keyring/kali-archive-keyring_2018.2_all.deb'
-echo 'apt install ./kali-archive-keyring_2018.2_all.deb -y'
-echo 'apt-get update && apt-get full-upgrade -y && apt-get install kali-linux-nethunter -y'  
+#echo ''
+#echo ''
+#echo 'now run nethunter with tsudo startkali and put some commands there:'
+#echo ''
+#echo 'apt update && apt install wget openssl'
+#echo 'wget https://http.kali.org/kali/pool/main/k/kali-archive-keyring/kali-archive-keyring_2018.2_all.deb'
+#echo 'apt install ./kali-archive-keyring_2018.2_all.deb -y'
+#echo 'apt-get update && apt-get full-upgrade -y && apt-get install kali-linux-nethunter -y'  
