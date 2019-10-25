e911eef41973d7c50eb0bb139d52ea4a35e1bbf3
Ivan Topor <49291467+l4ckyguy@users.noreply.github.com>
2019-10-25 17:37:54 +0300

    Create wire.sh

---
+++ wire.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ssh -t root@breguet.pw 'wget https://raw.githubusercontent.com/l-n-s/wireguard-install/master/wireguard-install.sh -O wireguard-install.sh
+bash wireguard-install.sh'
+
+scp root@breguet.pw:/root/*-wg.conf  ./storage/
